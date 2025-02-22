7961a0a4784b84a15046ce4642eb5022269ea5dd
Levi Eber Gomes Barbosa <gblevieber@gmail.com>
2025-02-22 19:39:31 -0300

    chore: configure prettier and eslint

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] place-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <h1 className="text-2xl font-bold text-rose-400">StoreIt</h1>
     </div>
   );
